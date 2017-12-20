50b071e38668b4224790591652d73b7c45d73a18
jaannis <janis.zalitis.ba@gmail.com>
2017-12-20 21:21:17 +0200

    sup

---
+++ RCS/smartercalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartercalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //izveido kalkulatora objektu
+
+            //paprasīt lietotājam ievadīt ievadi
+            Console.WriteLine("Please enter darbība");
+            string input = Console.ReadLine();
+
+            //1+2+3 skaits ir 5, pēdējā simbola pozīcija ir 4
+            // 1+ skaits ir 2 pēdējā pozīcijā ir 1
+            int result;
+            int counter = 0;
+            while(counter < input.Lenght)
+            {
+                char symbol = input[counter];
+                if(symbol == '+')
+                {
+                    Console.WriteLine("plus");
+                }
+                else
+                {
+                    int number;
+                    number = Int32.Parse(symbol.ToString());
+                    Console.WriteLine("number" + number);
+                }
+                counter = counter + 1;
+                
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

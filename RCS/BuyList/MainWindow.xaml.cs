ca3e4c51d6f2d4bdc88ed7f1b64ac84ac321cb1f
jaannis <janis.zalitis.ba@gmail.com>
2017-12-21 20:34:45 +0200

    kaut kas jauns

---
+++ RCS/BuyList/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BuyList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<string> BuyItemsList = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BuyListItemName.Text = "Lūdzu ievadiet pirkumu";
+
+            //pasakam BuyItemsControl, ka ir jāizmanto mūsu saraksts,
+            //kā rādāmo lietu avots (jāskatās no saraksta, ko rādīt)
+            this.BuyItemListControl.ItemsSource = this.BuyItemsList;
+            
+        }
+
+        private void AddListItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            //izvelkam vērtību no teksta lauka
+           string enteredItemToBuy =  this.BuyListItemName.Text;
+
+            //nodzēšam vertību teksta laukā
+           this.BuyListItemName.Text = "";
+
+            //ierakstam iegūto vētību teksta blokā
+            this.BuyItemsList.Add(enteredItemToBuy);
+        }
+    }
+}
--- RCS/smartercalculator/Program.cs
+++ RCS/smartercalculator/Program.cs
@@ -4,29 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace smartercalculator
+namespace SmarterCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // izveido kalkulatora objektu
+            MathParser parser;
+            parser = new MathParser();
 
-            //izveido kalkulatora objektu
-            math parser;
-            parser = new math();
-
-            //paprasīt lietotājam ievadīt ievadi
-            Console.WriteLine("Please enter darbība");
+            // paprasīt lietotājam ievadīt ievadi
+            Console.WriteLine("please enter darbība");
             string input = Console.ReadLine();
 
-            //izsauc aprēķināšanas funkciju un saglabā rezultātu
+            // izsauc aprēķināšanas funkciju un saglabā rezultātu
             int result = parser.ParseMath(input);
+            if (result == 0)
+            {
+                Console.WriteLine("operation unsucessful");
+            }
 
-            //izvada rezultātu uz ekrāna
-            Console.WriteLine("Your result" + result);
-
+            // izvada rezultātu uz ekrāna
+            Console.WriteLine("your result " + result);
             Console.ReadLine();
-
         }
     }
 }

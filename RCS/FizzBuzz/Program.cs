ea2e9e90aab0f2d39247f967b6a07706bf9df58b
jaannis <janis.zalitis.ba@gmail.com>
2017-12-29 19:31:59 +0200

    turpinajums

--- RCS/FizzBuzz/Program.cs
+++ RCS/FizzBuzz/Program.cs
@@ -10,31 +10,41 @@ namespace eksperments
     {
         static void Main(string[] args)
         {
+            //Ievadam konsolē skaitļus
             Console.WriteLine("Ievadi skaitli");
             string input = Console.ReadLine();
 
-
+            //Pārvēršam to skaitli par int
             int number;
             Int32.TryParse(input, out number);
 
+            //Paņemam no konsoles skaitli un no tā uztaisam range
             IEnumerable<int> skaitlis = Enumerable.Range(1, number);
 
+            //Meklējam Fizus un Buzus
             foreach (int num in skaitlis)
             {
+
+                //Meklējam FizzBuzzu, nezinu kāpēc liek %
+                //bet tā daudz kur internetā bija
                 if (num % 3 == 0 && num % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
 
+                //Atrodam Fizu
                 else if (num % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
 
+                //Atrodam Bazu
                 else if (num % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
+
+                //Palaižam pārējos skaitļus
                 else
                 {
                     Console.WriteLine(num);
---
+++ RCS/WordFinder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // paprasīt lietotājam ievadīt tekstu
+            Console.WriteLine("Ievadi vārdu");
+            string input = Console.ReadLine();
+
+            // iegūt no ievadītā teksta visus unikālos burtus
+
+
+            // izveidot mainīgo, kurā glabāsim tekstu, kas saturēs unikālos burtus
+            // ciklā iet cauri lietotāja ievadītajam tekstam pa vienam burtam
+            // ja šis burts neatrodas mūsu izveidotajā unikālo burtu teksta mainīgajā, tad pievienot šo burtu šim teksta mainīgajam
+            // kad viss teksts apskatīts, atgriezt rezultāta teksta mainīgo ar unikālajiem burtiem
+            // nolasīt visus vārdus no faila, ierakstot tos sarakstā, kur katrs ieraksts ir viens vārds
+            // iet cauri sarakstam pa vienam vārdam
+            // iet cauri šobrīdējam vārdam no faila pa vienam simbolam
+            // pārbaudīt, vai šis simbols atrodas mūsu unikālo burtu tekstā
+            // ja simbols neatrodas starp unikālajiem simboliem, tad pārstāt iet cauri šim vārdam pa simboliem
+            // iet cauri unikālajiem simboliem pa vienam simbolam
+            // pārbaudīt, vai šis simbols atrodas mūsu vārda no faila tekstā
+            // ja simbols neatrodas vārdā no faila, tad pārstāt iet cauri šim vārdam
+            // ja visi simboli no vārda atrodas unikālo simbolu virknē un visi unikālie simboli atrodas vārdā, izvadīt to uz ekrāna
+
+        }
+    }
+}

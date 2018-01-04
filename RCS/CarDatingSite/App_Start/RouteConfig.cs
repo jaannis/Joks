3a23f2092a344eaf1574fb2c6f297c7eb49cb883
jaannis <janis.zalitis.ba@gmail.com>
2018-01-04 19:13:42 +0200

    CatsController

--- RCS/CarDatingSite/App_Start/RouteConfig.cs
+++ RCS/CarDatingSite/App_Start/RouteConfig.cs
@@ -14,10 +14,15 @@ namespace CarDatingSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: "KakuCels",
+                url: "kaki/{action}/{id}",
+                defaults: new { controller = "Cats", action = "Index", id = UrlParameter.Optional });
+
+            routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        );
         }
     }
 }
